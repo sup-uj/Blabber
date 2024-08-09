15da907dd3a6e71a4e5eb720584e16eeb03f3d55
Utkarsh Jaiswal <starutkarshj017@gmail.com>
2024-08-09 16:21:13 +0530

    Search Frontend done

--- client/src/component/specific/Search.jsx
+++ client/src/component/specific/Search.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useInputValidation } from "6pp";
+import { Search as SearchIcon } from "@mui/icons-material";
+import {
+    Dialog,
+    DialogTitle,
+    InputAdornment,
+    List,
+    Stack,
+    TextField,
+} from "@mui/material";
+import { sampleUsers } from '../../constants/sampleData';
+import UserItem from '../shared/UserItem';
+
+
 
 const Search = () => {
-  return (
-    <div>
-      Search
-    </div>
-  )
-}
 
-export default Search
+    const search = useInputValidation("");
+
+    let isLoadingSendFriendRequest=false;
+
+    const [users, setUsers] = useState(sampleUsers);
+
+    const addFriendHandler = async (id) => {
+        console.log(id);
+    };
+
+    return (
+        <Dialog open >
+            <Stack p={"2rem"} direction={"column"} width={"25rem"}>
+                <DialogTitle textAlign={"center"}>Find People</DialogTitle>
+                <TextField
+                    label=""
+                    value={search.value}
+                    onChange={search.changeHandler}
+                    variant="outlined"
+                    size="small"
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <SearchIcon />
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+
+                <List>
+                    {users.map((i) => (
+                        <UserItem
+                            user={i}
+                            key={i._id}
+                            handler={addFriendHandler}
+                            handlerIsLoading={isLoadingSendFriendRequest}
+                        />
+                    ))}
+                </List>
+            </Stack>
+        </Dialog>
+    );
+};
+
+export default Search;
--- client/src/constants/sampleData.js
+++ client/src/constants/sampleData.js
@@ -14,3 +14,16 @@ export const samepleChats = [
       members: ["1", "2"],
     }
 ]
+
+export const sampleUsers = [
+  {
+    avatar: "https://www.w3schools.com/howto/img_avatar.png",
+    name: "John Doe",
+    _id: "1",
+  },
+  {
+    avatar: "https://www.w3schools.com/howto/img_avatar.png",
+    name: "John Boi",
+    _id: "2",
+  },
+];
